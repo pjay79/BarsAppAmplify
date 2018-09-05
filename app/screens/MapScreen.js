665330bdfe3e85cf574e1ccc6acfd8a112eb68b5
pjay79 <praveen.jayarajan@gmail.com>
2018-09-05 23:03:44 +1000

    Remove file

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 import Config from 'react-native-config';
+import * as COLORS from '../config/colors';
 
 Mapbox.setAccessToken(Config.MAPBOX_ACCESS_TOKEN);
 
@@ -10,15 +12,57 @@ export default class MapScreen extends Component {
     header: null,
   };
 
+  state = {
+    latitude: '',
+    longitude: '',
+  };
+
+  componentDidMount() {
+    this.getUserLocation();
+  }
+
+  getUserLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState(
+          {
+            latitude: parseFloat(position.coords.latitude),
+            longitude: parseFloat(position.coords.longitude),
+          },
+          () => {
+            console.log('User location: ', this.state.latitude, this.state.longitude);
+          },
+        );
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+  };
+
+  renderAnnotations = () => (
+    <Mapbox.PointAnnotation key="pointAnnotation" id="pointAnnotation" coordinate={[11.256, 43.77]}>
+      <View style={styles.annotationContainer}>
+        <View style={styles.annotationFill} />
+      </View>
+      <Mapbox.Callout title="You are here" />
+    </Mapbox.PointAnnotation>
+  );
+
   render() {
+    const { latitude, longitude } = this.state;
+
     return (
       <View style={styles.container}>
         <Mapbox.MapView
-          styleURL={Mapbox.StyleURL.Street}
-          zoomLevel={15}
-          centerCoordinate={[11.256, 43.77]}
+          styleURL={Mapbox.StyleURL.Dark}
+          zoomLevel={12}
+          // centerCoordinate={[-122.431, 37.774]}
+          centerCoordinate={[longitude, latitude]}
           style={styles.container}
-        />
+          showUserLocation
+        >
+          {this.renderAnnotations()}
+        </Mapbox.MapView>
       </View>
     );
   }
@@ -27,5 +71,20 @@ export default class MapScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.TEXT_PRIMARY_COLOR,
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: COLORS.ACCENT_COLOR,
+    transform: [{ scale: 0.6 }],
   },
 });
