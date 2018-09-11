b18698939ae2f2ec74759dc57d0a056964a6da27
pjay79 <praveen.jayarajan@gmail.com>
2018-09-11 22:46:14 +1000

    Refactor MoreScreen

--- app/screens/MoreScreen.js
+++ app/screens/MoreScreen.js
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import Auth from 'aws-amplify';
-import Button from '../components/Button';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { Auth } from 'aws-amplify';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as COLORS from '../config/colors';
 
 export default class MoreScreen extends Component {
@@ -33,11 +41,57 @@ export default class MoreScreen extends Component {
     const { loading } = this.state;
     return (
       <View style={styles.container}>
-        <Button
-          title="Sign Out"
-          onPress={this.signOut}
-          style={{ backgroundColor: COLORS.DEFAULT_PRIMARY_COLOR }}
-        />
+        <TouchableOpacity onPress={() => console.log('Go to ABOUT')}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>
+About
+            </Text>
+            <Ionicons
+              name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
+              size={20}
+              color={COLORS.DIVIDER_COLOR}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log('Go to CONTACT US')}>
+          <View
+            style={[
+              styles.item,
+              {
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+              },
+            ]}
+          >
+            <Text style={styles.itemText}>
+Contact Us
+            </Text>
+            <Ionicons
+              name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
+              size={20}
+              color={COLORS.DIVIDER_COLOR}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log('Go to FEEDBACK')}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>
+Send Feedback
+            </Text>
+            <Ionicons
+              name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
+              size={20}
+              color={COLORS.DIVIDER_COLOR}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.signOut}>
+          <View style={styles.itemSignOut}>
+            <Text style={styles.itemTextSignOut}>
+SIGN OUT
+            </Text>
+          </View>
+        </TouchableOpacity>
         {loading && <ActivityIndicator color={COLORS.PRIMARY_TEXT_COLOR} />}
       </View>
     );
@@ -47,8 +101,43 @@ export default class MoreScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 20,
+    backgroundColor: COLORS.BACKGROUND_COLOR,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: COLORS.TEXT_PRIMARY_COLOR,
+    width: Dimensions.get('window').width,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderTopColor: COLORS.DIVIDER_COLOR,
+    borderBottomColor: COLORS.DIVIDER_COLOR,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  itemText: {
+    fontWeight: '400',
+    letterSpacing: 2,
+  },
+  itemSignOut: {
+    backgroundColor: COLORS.TEXT_PRIMARY_COLOR,
+    width: Dimensions.get('window').width,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    marginVertical: 15,
+    borderTopColor: COLORS.DIVIDER_COLOR,
+    borderBottomColor: COLORS.DIVIDER_COLOR,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  itemTextSignOut: {
+    textAlign: 'center',
+    fontWeight: '800',
+    letterSpacing: 2,
+    color: COLORS.DEFAULT_PRIMARY_COLOR,
   },
 });
 
