e789a32f45c5f29f6f5c754c5dc465bf0b975269
pjay79 <praveen.jayarajan@gmail.com>
2018-12-08 22:03:02 +1100

    Update ios pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/pjay79/BarsAppAmplify-aws.git tmp 
-cd tmp && mv aws-exports ../
-rmdir tmp
+git clone https://github.com/pjay79/BarsAppAmplify-aws.git && mv BarsAppAmplify-aws/* BarsAppAmplify
