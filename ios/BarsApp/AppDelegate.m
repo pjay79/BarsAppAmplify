1f5484f4be20ab9e352b74483dbd3f79c768c045
pjay79 <praveen.jayarajan@gmail.com>
2018-12-21 23:03:43 +1100

    Update ios settings

--- ios/BarsApp/AppDelegate.m
+++ ios/BarsApp/AppDelegate.m
@@ -22,8 +22,8 @@
     #ifdef DEBUG
         jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
     #else
-        jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-        // jsCodeLocation = [CodePush bundleURL];
+        // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+        jsCodeLocation = [CodePush bundleURL];
     #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
